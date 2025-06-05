dd2f6b874f260caf8190ed086e5f2fc596478a16
NgocPhat003 <ngocphat2405@gmail.com>
2025-06-05 21:22:53 +0700

    add storage.js which is a module used to manage data using localStorage

---
+++ src/storage.js
@@ -0,0 +1,35 @@
+// Module to store data using localStorage
+
+const STORAGE_KEY = 'todoAppProjects';
+
+const storage = {
+    savedata: function (data) {
+        try {
+            const serializedData =JSON.stringify(data);
+            localStorage.setItem(STORAGE_KEY, serializedData);
+        }catch(error) {
+            console.error('Error when trying to save data to localStorage');
+        }
+    },
+
+    loadData: function () {
+        try {
+            const serializedData = localStorage.getItem(STORAGE_KEY);
+            if (serializedData === null) {
+                return undefined;
+            }
+            return JSON.parse(serializedData);
+        } catch (error) {
+            console.error('Error when trying to load data from localStorage');
+            return undefined;
+        }
+    },
+
+    clearData: function () {
+        try{
+            localStorage.removeItem(STORAGE_KEY);
+        }catch (error) {
+            console.error('Error when trying to clear data from localStorage');
+        }
+    },
+};
